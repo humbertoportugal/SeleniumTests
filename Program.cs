360ca927edda344e1778985d1af481183f293fe1
humberto.portugal <humberto.portugal@PRT1AWKSHDSK007>
2020-10-13 16:34:24 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace SeleniumAutomation
+{
+    class Program
+    {
+        //Initialize a new instance of the Chromedriver
+        IWebDriver driver = new ChromeDriver("D:\\transferencias\\chromedriver_win32");
+        
+
+        static void Main()
+        {
+
+        }
+        [SetUp]
+        public void Initialize()
+        {
+            //Navigate to ToolsQA page
+            driver.Navigate().GoToUrl("https://demoqa.com/");
+            driver.Manage().Window.Maximize();
+            Console.WriteLine("Opened URL");
+        }
+        [Test]
+        public void ExecuteTest()
+        {
+            SeleniumSetMethods.Click(driver, "/html/body/div/div/div/div[2]/div/div[1]", "XPath");
+            SeleniumSetMethods.Click(driver, "/html/body/div/div/div/div[2]/div[1]/div/div/div[1]/div/ul/li[1]", "XPath");
+            SeleniumSetMethods.EnterText(driver, "userName", "Test Automation 1.0", "Id");
+            SeleniumSetMethods.Clear(driver,"userName", "Id");
+            SeleniumSetMethods.EnterText(driver, "userName", "Test Automation 1.1", "Id");
+            Console.WriteLine("Entered Test Automation 1.0, cleared and entered Test Automation 1.1");
+        }
+        [TearDown]
+        public void CleanUp()
+        {
+            //Close the driver
+            driver.Close();
+            Console.WriteLine("Closed the Browser");
+        }
+    }
+}
---
+++ SeleniumSetMethods.cs
@@ -0,0 +1,62 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumAutomation
+{
+    class SeleniumSetMethods
+    {
+        //Enter Text
+        public static void EnterText(IWebDriver driver,string element, string value, string elementtype)
+        {
+            if (elementtype == "Id")
+                driver.FindElement(By.Id(element)).SendKeys(value);
+            if (elementtype == "Name")
+                driver.FindElement(By.Name(element)).SendKeys(value);
+            if (elementtype == "Class")
+                driver.FindElement(By.ClassName(element)).SendKeys(value);
+            if (elementtype == "XPath")
+                driver.FindElement(By.XPath(element)).SendKeys(value);
+        }
+
+        //Click buttons, checkboxs, options, etc
+        public static void Click(IWebDriver driver, string element, string elementtype)
+        {
+            if (elementtype == "Id")
+                driver.FindElement(By.Id(element)).Click();
+            if (elementtype == "Name")
+                driver.FindElement(By.Name(element)).Click();
+            if (elementtype == "Class")
+                driver.FindElement(By.ClassName(element)).Click();
+            if (elementtype == "XPath")
+                driver.FindElement(By.XPath(element)).Click();
+        }
+
+        //Selecting drop down
+        public static void SelectDropDown(IWebDriver driver, string element, string value,string elementtype)
+        {
+            if (elementtype == "Id")
+                new SelectElement(driver.FindElement(By.Id(element))).SelectByText(value);
+            if (elementtype == "Name")
+                new SelectElement(driver.FindElement(By.Name(element))).SelectByText(value);
+            if (elementtype == "Class")
+                new SelectElement(driver.FindElement(By.ClassName(element))).SelectByText(value);
+            if (elementtype == "XPath")
+                new SelectElement(driver.FindElement(By.XPath(element))).SelectByText(value);
+        }
+
+        public static void Clear(IWebDriver driver, string element, string elementtype)
+        {
+            if (elementtype == "Id")
+                driver.FindElement(By.Id(element)).Clear();
+            if (elementtype == "Name")
+                driver.FindElement(By.Name(element)).Clear();
+            if (elementtype == "Class")
+                driver.FindElement(By.ClassName(element)).Clear();
+            if (elementtype == "XPath")
+                driver.FindElement(By.XPath(element)).Clear();
+        }
+    }
+}
