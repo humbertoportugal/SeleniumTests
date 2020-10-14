e80c7ffe83bf1c7dfd4bbfc70d06d37509158c50
humberto.portugal <humberto.portugal@PRT1AWKSHDSK007>
2020-10-14 16:47:45 +0100

    Implemented new features and have removed some code smells.

--- SeleniumGetMethods.cs
+++ SeleniumGetMethods.cs
@@ -9,28 +9,28 @@ namespace SeleniumAutomation
 {
     class SeleniumGetMethods
     {
-        public static string GetText(string element, string elementtype)
+        public static string GetText(string element, PropertyType elementtype)
         {
-            if (elementtype == "Id")
+            if (elementtype == PropertyType.Id)
                 return PropertiesCollection.driver.FindElement(By.Id(element)).Text;
-            if (elementtype == "Name")
+            if (elementtype == PropertyType.Name)
                 return PropertiesCollection.driver.FindElement(By.Name(element)).Text;
-            if (elementtype == "Class")
+            if (elementtype == PropertyType.ClassName)
                 return PropertiesCollection.driver.FindElement(By.ClassName(element)).Text;
-            if (elementtype == "XPath")
+            if (elementtype == PropertyType.XPath)
                 return PropertiesCollection.driver.FindElement(By.XPath(element)).Text;
             else return String.Empty;
         }
 
-        public static string GetFromField(string element, string elementtype)
+        public static string GetFromField(string element, PropertyType elementtype)
         {
-            if (elementtype == "Id")
+            if (elementtype == PropertyType.Id)
                 return PropertiesCollection.driver.FindElement(By.Id(element)).GetAttribute("value");
-            if (elementtype == "Name")
+            if (elementtype == PropertyType.Name)
                 return PropertiesCollection.driver.FindElement(By.Name(element)).GetAttribute("value");
-            if (elementtype == "Class")
+            if (elementtype == PropertyType.ClassName)
                 return PropertiesCollection.driver.FindElement(By.ClassName(element)).GetAttribute("value");
-            if (elementtype == "XPath")
+            if (elementtype == PropertyType.XPath)
                 return PropertiesCollection.driver.FindElement(By.XPath(element)).GetAttribute("value");
             else return String.Empty;
         }
